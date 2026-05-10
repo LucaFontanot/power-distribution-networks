38de5a218e98825d39b8f6fed9f1e3fe5ea6e32e
Luca Fontanot <contact@lucafontanot.it>
2026-05-11 00:30:56 +0200

    rf: put plotting code inside plot.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+import time
+
 from generator import Generator
+from plot import plot_network
+from solver.LF_SE import solve_lf_se
+from solver.LF_SCF import solve_lf_scf
+from solver.OL_SE import solve_ol_se, solve_ol_se_cc
+
 
 CASE_STUDIES = {
     "Case54": dict(
@@ -22,22 +29,39 @@ CASE_STUDIES = {
         density_center=6.83, density_suburbs=1.50,
     ),
 }
-SEED = 40
+SEED = 57
 
 def run_case(generator, case):
-    for i in range(100):
-        data = generator.generate(
-            case["n_ps_center"],
-            case["n_ps_suburbs"],
-            case["n_ss_center"],
-            case["n_ss_suburbs"],
-            case["density_center"],
-            case["density_suburbs"],
-            i,
-        )
-        generator.plot(data)
+    data = generator.generate(
+        case["n_ps_center"],
+        case["n_ps_suburbs"],
+        case["n_ss_center"],
+        case["n_ss_suburbs"],
+        case["density_center"],
+        case["density_suburbs"],
+        SEED
+    )
+    plot_network(data, show_arc_costs=False)
+    plot_network(data, show_arc_costs=True)
+    time_ms = time.time()
+    arcs, obj = solve_lf_scf(data, time_limit=21600, verbose=False)
+    print(f"LF-SCF: time={time.time() - time_ms:.2f} seconds")
+    time_ms = time.time()
+    arcs, obj = solve_lf_se(data, time_limit=21600, verbose=False)
+    print(f"LF-SE: time={time.time() - time_ms:.2f} seconds")
+    time_ms = time.time()
+    arcs, obj = solve_ol_se(data, time_limit=21600, verbose=False)
+    print(f"OL-SCF: time={time.time() - time_ms:.2f} seconds")
+    time_ms = time.time()
+    arcs, obj = solve_ol_se_cc(data, time_limit=21600, verbose=False)
+    print(f"OL-SCF: time={time.time() - time_ms:.2f} seconds")
+
+
 
 
 if __name__ == "__main__":
     gen = Generator()
     run_case(gen, CASE_STUDIES["Case54"])
+    #run_case(gen, CASE_STUDIES["Case78"])
+    #run_case(gen, CASE_STUDIES["Case104"])
+    #run_case(gen, CASE_STUDIES["Case154"])
